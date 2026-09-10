4c2f45be5f956b973171c184874dc36a87d79b2e
kimbomin <bomin0322s2@gmail.com>
2026-09-10 15:34:49 +0900

    feat: RefundRepository, RefundItemRepository 추가

--- src/main/java/com/example/commerce/domain/refund/repository/RefundItemRepository.java
+++ src/main/java/com/example/commerce/domain/refund/repository/RefundItemRepository.java
@@ -1,4 +1,7 @@
 package com.example.commerce.domain.refund.repository;
 
-public class RefundItemRepository {
+import com.example.commerce.domain.refund.entity.RefundItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefundItemRepository extends JpaRepository<RefundItem, Long> {
 }
