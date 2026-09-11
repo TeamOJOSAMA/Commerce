6855da92ee9d81bc23d5b362e60ed782de3cc902
GaryKang0208 <khb1661@gmail.com>
2026-09-11 19:10:39 +0900

    feat: NEGATIVE_PRICE INVALID_PRICE_RANGE EVENT_NOT_FOUND 새로운 에러코드 구현

--- src/main/java/com/example/commerce/common/exception/ErrorCode.java
+++ src/main/java/com/example/commerce/common/exception/ErrorCode.java
@@ -30,10 +30,13 @@ public enum ErrorCode {
     PRICE_ERROR(HttpStatus.BAD_REQUEST,"PRODUCT_5","최소 가격이 최대 가격보다 클수 없습니다"),
     STOCK_AMOUNT_OVERFLOW(HttpStatus.BAD_REQUEST, "PRODUCT_006", "재고 수량이 허용 범위를 초과했습니다."),
     INVALID_EVENT_STOCK(HttpStatus.BAD_REQUEST, "PRODUCT_007", "이벤트 재고가 설정되지 않았습니다."),
+    NEGATIVE_PRICE(HttpStatus.BAD_REQUEST, "PRODUCT_008","가격은 0 이상이어야 합니다."),
+    INVALID_PRICE_RANGE(HttpStatus.BAD_REQUEST, "PRODUCT_009","최소 가격이 최대 가격보다 클 수 없습니다."),
 
     // 이벤트
     INVALID_EVENT_PRICE(HttpStatus.BAD_REQUEST,"EVENT_001", "이벤트중인 상품가격이 필요합니다"),
     EVENT_INFO_REQUIRED(HttpStatus.BAD_REQUEST,"EVENT_002", "이벤트 세부 내용이 없습니다"),
+    EVENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "EVENT_003","이벤트 상품을 찾을수 없습니다"),
     // 장바구니
     CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "CART_001","장바구니 상품을 찾을 수 없습니다."),
     CART_EMPTY(HttpStatus.NOT_FOUND, "CART_002", "장바구니가 비어있습니다."),
