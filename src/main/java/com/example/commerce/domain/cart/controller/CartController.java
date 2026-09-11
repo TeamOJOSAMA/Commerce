6a7aa79418aa174bc78a7f37fc9ce7fac1d48f5c
yeang976-art <yeang976@gmail.com>
2026-09-11 19:52:01 +0900

    feat: 요청한 회원 소유의 장바구니의 특정 상품 삭제 API 구현

--- src/main/java/com/example/commerce/domain/cart/controller/CartController.java
+++ src/main/java/com/example/commerce/domain/cart/controller/CartController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,13 +37,23 @@ public class CartController {
             @PathVariable Long productId,
             @Valid @RequestBody AddToCartRequest request
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.ok(cartFacade.addCartItem(authUser.getUserId(), productId, request)));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse
+                        .ok(
+                                "해당 상품을 장바구니에 담았습니다.",
+                                cartFacade.addCartItem(authUser.getUserId(), productId, request)
+                        ));
     }
 
     @GetMapping
     public ResponseEntity<ApiResponse<GetCartResponse>> getCart(@AuthenticationPrincipal AuthUser authUser) {
-        return ResponseEntity.ok(ApiResponse.ok(cartService.getCart(authUser.getUserId())));
+        return ResponseEntity
+                .ok(ApiResponse
+                        .ok(
+                                "장바구니에 있는 상품 목록을 열었습니다.",
+                                cartService.getCart(authUser.getUserId())
+                        ));
     }
 
     @PatchMapping("/{cartItemId}")
@@ -52,6 +63,22 @@ public class CartController {
             @Valid @RequestBody UpdateQuantityRequest request
     ) {
         return ResponseEntity
-                .ok(ApiResponse.ok(cartService.updateQuantity(authUser.getUserId(), cartItemId, request)));
+                .ok(ApiResponse
+                        .ok(
+                                "해당 상품의 수량을 변경했습니다.",
+                                cartService.updateQuantity(authUser.getUserId(), cartItemId, request)
+                        ));
+    }
+
+    @DeleteMapping("/{cartItemId}")
+    public ResponseEntity<ApiResponse<Void>> deleteCartItem(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PathVariable Long cartItemId
+    ) {
+        cartService.deleteCartItem(authUser.getUserId(), cartItemId);
+
+        return ResponseEntity.noContent().build(); // 204
+
+        // 참고로 204 No Content는 응답 본문을 포함하지 않으므로 상태 코드만 반환
     }
 }
